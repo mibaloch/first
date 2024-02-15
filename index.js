ee8965b0a93556115d17e8325e80d5dbbe16eabf
mibaloch <157287033+mibaloch@users.noreply.github.com>
2024-02-15 18:27:40 +0500

    first backend added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/a', (req, res) => {
+  res.send('First: A!')
+})
+
+app.get('/b', (req, res) => {
+    res.send('First: B!')
+  })
+  
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
